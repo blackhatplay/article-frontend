23ec590042eee1ba806a1a898b5c88c0b82666e2
blackhatplay <mannuall4u@gmail.com>
2021-04-10 16:46:35 +0530

    server url

--- src/api/server.js
+++ src/api/server.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-export const serverURL =
-  process.env.NODE_ENV === "development"
-    ? process.env.REACT_APP_SERVER
-    : process.env.SERVER;
+export const serverURL = process.env.REACT_APP_SERVER;
 
 export default axios.create({
   baseURL: serverURL,
